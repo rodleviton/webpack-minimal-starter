6941c1e272c0d5f545d989a822534d08e8252d31
Rod Leviton <rod@rodlevtion.com>
2016-10-18 23:08:47 +1100

    initial commit

---
+++ src/app.test.js
@@ -0,0 +1,5 @@
+describe('controller', () => {
+  it('can be created', () => {
+    expect(1).to.equal(1)
+  })
+})
---
+++ webpack.config.babel.js
@@ -0,0 +1,58 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpackValidator = require('webpack-validator')
+const { getIfUtils, removeEmpty } = require('webpack-config-utils')
+const OfflinePlugin = require('offline-plugin')
+
+module.exports = env => {
+  const { ifProd, ifNotProd } = getIfUtils(env)
+  const config = webpackValidator({
+    context: resolve(__dirname, 'src'),
+    entry: './app.js',
+    output: {
+      filename: ifProd('bundle.[name].[chunkhash].js', 'bundle.[name].js'),
+      path: resolve(__dirname, 'dist'),
+      pathinfo: ifNotProd(),
+    },
+    devtool: ifProd('source-map', 'eval'),
+    bail: env.prod,
+    module: {
+      loaders: [
+        { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ }, {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: 'style',
+            loader: 'css',
+          })
+        },
+      ],
+    },
+    plugins: removeEmpty([
+      new ProgressBarPlugin(),
+      new ExtractTextPlugin(ifProd('styles.[name].[chunkhash].css', 'styles.[name].css')),
+      ifProd(new InlineManifestWebpackPlugin()),
+      ifProd(new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest'],
+      })),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'head',
+      }),
+      new OfflinePlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: ifProd('"production"', '"development"')
+        }
+      }),
+    ]),
+  })
+  if (env.debug) {
+    console.log(config)
+    debugger // eslint-disable-line
+  }
+  return config
+}
